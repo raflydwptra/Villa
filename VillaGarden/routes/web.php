b55efea196b5418e64d528eea693abbd0bb44e43
Afta2003 <aftabestz@gmail.com>
2023-12-16 20:43:33 +0700

    revisi

--- VillaGarden/routes/web.php
+++ VillaGarden/routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+Route::get('/login', function () {
+    return view ('login');
+});
+
+Route::get('/Sign up', function () {
+    return view ('Sign up');
+});
+
+
