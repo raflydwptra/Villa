964af7be0b49a063b3a60d7bbbe0bfcb9a6aef67
raflydwptra <raflydwiputra7@gmail.com>
2023-12-16 19:53:09 +0700

    revisi gambar fasilitas

--- VillaGarden/routes/web.php
+++ VillaGarden/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/Sign Up', function () {
+    return view('Sign Up');
 });
