344cc6e4cc589889b32c72e616fd7d68ffd2a3b1
raflydwptra <raflydwiputra7@gmail.com>
2023-12-16 19:45:32 +0700

    revisi gambar fasilitas

--- VillaGarden/resources/views/welcome.blade.php
+++ VillaGarden/resources/views/welcome.blade.php
@@ -329,154 +329,113 @@
                     <div class="col-lg-12 d-flex justify-content-center">
                         <ul id="portfolio-flters">
                             <li data-filter="*" class="filter-active">All</li>
-                            <li data-filter=".filter-app">App</li>
-                            <li data-filter=".filter-card">Card</li>
-                            <li data-filter=".filter-web">Web</li>
+                            <li data-filter=".filter-indoor">Indoor</li>
+                            <li data-filter=".filter-outdoor">Outdoor</li>
                         </ul>
                     </div>
                 </div>
 
                 <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-indoor">
                         <div class="portfolio-wrap">
-                            <img src="assets\img\portfolio\portfolio-1.jpg" class="img-fluid" alt="">
+                            <img src="{{ asset('Gp/assets/img/RuangTamu.jpg')}}" class="img-fluid" alt="" width="416" height="225">
                             <div class="portfolio-info">
-                                <h4>App 1</h4>
-                                <p>App</p>
+                                <h4>Ruang Tamu</h4>
                                 <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
-                                        class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i
-                                            class="bx bx-link"></i></a>
+                                    <a href="{{ asset('Gp/assets/img/RuangTamu.jpg')}}" data-gallery="portfolioGallery"
+                                        class="portfolio-lightbox" title="Ruang Tamu"><i class="bx bx-fullscreen"></i></a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <div class="col-lg-4 col-md-2 portfolio-item filter-indoor">
                         <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+                            <img src="{{ asset('Gp/assets/img/portfolio/Dapur.jpg')}}" class="img-fluid" alt="" width="416" height="225">
                             <div class="portfolio-info">
-                                <h4>Web 3</h4>
-                                <p>Web</p>
+                                <h4>Dapur</h4>
                                 <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery"
-                                        class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i
-                                            class="bx bx-link"></i></a>
+                                    <a href="{{ asset('Gp/assets/img/portfolio/Dapur.jpg')}}" data-gallery="portfolioGallery"
+                                        class="portfolio-lightbox" title="Dapur"><i class="bx bx-fullscreen"></i></a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-indoor">
                         <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
+                            <img src="{{ asset('Gp/assets/img/bakaran.jpg')}}" class="img-fluid" alt="" width="416" height="225">
                             <div class="portfolio-info">
-                                <h4>App 2</h4>
-                                <p>App</p>
+                                <h4>Ruang Kumpul</h4>
                                 <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery"
-                                        class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i
-                                            class="bx bx-link"></i></a>
+                                    <a href="{{ asset('Gp/assets/img/Bakaran.jpg')}}" data-gallery="portfolioGallery"
+                                        class="portfolio-lightbox" title="Ruang Kumpul"><i class="bx bx-fullscreen"></i></a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-outdoor">
                         <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
+                            <img src="{{ asset('Gp/assets/img/halamanbelakang.jpg')}}" class="img-fluid" alt="" width="416" height="225">
                             <div class="portfolio-info">
-                                <h4>Card 2</h4>
-                                <p>Card</p>
+                                <h4>Halaman Belakang</h4>
                                 <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery"
-                                        class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i
-                                            class="bx bx-link"></i></a>
+                                    <a href="{{ asset('Gp/assets/img/halamanbelakang.jpg')}}" data-gallery="portfolioGallery"
+                                        class="portfolio-lightbox" title="Halaman Belakang"><i class="bx bx-fullscreen"></i></a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-indoor">
                         <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+                            <img src="{{ asset('Gp/assets/img/kamar.jpg')}}" class="img-fluid" alt="" width="416" height="225">
                             <div class="portfolio-info">
-                                <h4>Web 2</h4>
-                                <p>Web</p>
+                                <h4>Kamar Tidur</h4>
                                 <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery"
-                                        class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i
-                                            class="bx bx-link"></i></a>
+                                    <a href="{{ asset('Gp/assets/img/kamar.jpg')}}" data-gallery="portfolioGallery"
+                                        class="portfolio-lightbox" title="Kamar Tidur"><i class="bx bx-fullscreen"></i></a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-outdoor">
                         <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
+                            <img src="{{ asset('Gp/assets/img/balcony.jpg')}}" class="img-fluid" alt="" width="416" height="225">
                             <div class="portfolio-info">
-                                <h4>App 3</h4>
-                                <p>App</p>
+                                <h4>Balcony</h4>
                                 <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery"
-                                        class="portfolio-lightbox" title="App 3"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i
-                                            class="bx bx-link"></i></a>
+                                    <a href="{{ asset('Gp/assets/img/balcony.jpg')}}" data-gallery="portfolioGallery"
+                                        class="portfolio-lightbox" title="Balcony"><i class="bx bx-fullscreen"></i></a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-outdoor">
                         <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
+                            <img src="Gp/assets/img/halaman.jpg" class="img-fluid" alt="" width="416" height="225">
                             <div class="portfolio-info">
-                                <h4>Card 1</h4>
-                                <p>Card</p>
+                                <h4>Halaman</h4>
                                 <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery"
-                                        class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i
-                                            class="bx bx-link"></i></a>
+                                    <a href="{{ asset('Gp/assets/img/halaman.jpg')}}" data-gallery="portfolioGallery"
+                                        class="portfolio-lightbox" title="Halaman"><i class="bx bx-fullscreen"></i></a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-outdoor">
                         <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
+                            <img src="{{ asset('Gp/assets/img/kolamrenang.jpg')}}" class="img-fluid" alt="" width="416" height="225">
                             <div class="portfolio-info">
-                                <h4>Card 3</h4>
-                                <p>Card</p>
+                                <h4>Kolam Renang</h4>
                                 <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery"
-                                        class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                        <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Web 3</h4>
-                                <p>Web</p>
-                                <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery"
-                                        class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i
-                                            class="bx bx-link"></i></a>
+                                    <a href="{{ asset('Gp/assets/img/kolamrenang.jpg')}}" data-gallery="portfolioGallery"
+                                        class="portfolio-lightbox" title="Kolam Renang"><i class="bx bx-fullscreen"></i></a>
                                 </div>
                             </div>
                         </div>
